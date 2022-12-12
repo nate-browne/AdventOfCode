5cf86b8a92df74c9385b117e086fb4da5fb6a0f6
Nate Browne <nate-browne@github.com>
2022-12-12 11:02:50 -0800

    day 11 part 2 python done

--- 2022/day11/day11.py
+++ 2022/day11/day11.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import json
+from math import prod
 from typing import Dict
 from collections import deque
 from sys import argv, exit, stderr
@@ -10,36 +11,39 @@ from sys import argv, exit, stderr
 expected_args = 2
 
 
-def parse_operation(op_str: str):
-    '''string is in format new=old<op>#\n
-    where <op> is in (%,*,+,-,/) and # is a numerical value.\n
-    This function returns a function
-    '''
-    # nasty little check for old * old
-    if op_str.split('=')[1] == 'old*old':
-        return lambda x: x * x
-
-    op_and_num = op_str.split('=')[1].split('old')[1]
-
-    match op_and_num[0]:
-        case '+':
-            return lambda x: x + int(op_and_num[1:])
-        case '-':
-            return lambda x: x - int(op_and_num[1:])
-        case '*':
-            return lambda x: x * int(op_and_num[1:])
-        case '/':
-            return lambda x: x // int(op_and_num[1:])
-        case '%':
-            return lambda x: x % int(op_and_num[1:])
-
-
 class Monkey(object):
+
+    @staticmethod
+    def parse_operation(op_str: str):
+        '''string is in format new=old<op>#\n
+        where <op> is in (%,*,+,-,/) and # is a numerical value.\n
+        This function returns a function
+        '''
+        # nasty little check for old * old
+        if op_str.split('=')[1] == 'old*old':
+            return lambda x: x * x
+
+        op_and_num = op_str.split('=')[1].split('old')[1]
+
+        match op_and_num[0]:
+            case '+':
+                return lambda x: x + int(op_and_num[1:])
+            case '-':
+                return lambda x: x - int(op_and_num[1:])
+            case '*':
+                return lambda x: x * int(op_and_num[1:])
+            case '/':
+                return lambda x: x // int(op_and_num[1:])
+            case '%':
+                return lambda x: x % int(op_and_num[1:])
+
     def __init__(self, monkey_dict: dict) -> Monkey:
         self.items = deque(monkey_dict['starting_items'])
-        self.operation = parse_operation(monkey_dict['operation'])
+        self.operation = Monkey.parse_operation(monkey_dict['operation'])
+        # needed in part 2. this is super gross but honestly idgaf
+        self.test_val = int(monkey_dict['test']['operation'].split('=')[1].split('%')[1])
         self.test = {
-            'operation': parse_operation(monkey_dict['test']['operation']),
+            'operation': Monkey.parse_operation(monkey_dict['test']['operation']),
             True: int(monkey_dict['test']['true']),
             False: int(monkey_dict['test']['false']),
         }
@@ -48,14 +52,17 @@ class Monkey(object):
     def __repr__(self) -> str:
         return f'{self.items}'
 
-    def inspect(self, other_monkeys: Dict[Monkey]) -> None:
+    def inspect(self, other_monkeys: Dict[Monkey], part1: bool, mod_val: int) -> None:
         '''Function that goes through the held items and performs the inspection
         '''
         while len(self.items):
             self.num_inspections += 1
             current_item = self.items.popleft()
             current_item = self.operation(current_item)
-            current_item //= 3
+            if part1:
+                current_item //= 3
+            else:
+                current_item %= mod_val
             check = self.test['operation'](current_item) == 0
             other_monkeys[self.test[check]].receive_item(current_item)
 
@@ -72,10 +79,13 @@ def parse_input_file(input_file: str) -> Dict[Monkey]:
     return monkeys
 
 
-def run_simulation(monkeys: Dict[Monkey], num_times: int) -> None:
-    for _ in range(num_times):
+def run_simulation(monkeys: Dict[Monkey], num_times: int, part1: bool) -> None:
+    mod_val = prod(monkey.test_val for monkey in monkeys.values())
+    for n in range(num_times):
         for monkey in monkeys.values():
-            monkey.inspect(monkeys)
+            monkey.inspect(monkeys, part1, mod_val)
+        if n % 1_000 == 0 and n != 0:
+            print('.')
 
 
 def main(input_file: str):
@@ -84,11 +94,18 @@ def main(input_file: str):
         exit(1)
 
     monkeys = parse_input_file(input_file)
-    run_simulation(monkeys, 20)
+    run_simulation(monkeys, 20, True)
 
     num_inspections = [m.num_inspections for m in monkeys.values()]
     num_inspections.sort(reverse=True)
-    print(f'Part 1: {num_inspections[0] * num_inspections[1]}')
+    print(f'Part 1: {prod(num_inspections[:2])}')
+
+    monkeys = parse_input_file(input_file)
+    run_simulation(monkeys, 10_000, False)
+
+    num_inspections = [m.num_inspections for m in monkeys.values()]
+    num_inspections.sort(reverse=True)
+    print(f'Part 2: {prod(num_inspections[:2])}')
 
 
 if __name__ == "__main__":
